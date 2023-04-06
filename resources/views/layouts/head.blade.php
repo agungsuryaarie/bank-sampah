602f99669d6258e514b1eea32cb5dfc4264a2237
agungsuryaarie <agungbahari3007@gmail.com>
2023-04-06 09:52:52 +0700

    add

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,7 +5,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"
+        rel="stylesheet">
+    <link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet">
+    <link href="{{ url('public/plugins/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ url('public/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+    <link href="{{ url('public/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}" rel="stylesheet">
+    <link href="{{ url('public/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}" rel="stylesheet">
+    <link href="{{ url('public/plugins/select2/css/select2.min.css') }}" rel="stylesheet">
+    <link href="{{ url('public/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}" rel="stylesheet">
+    <link href="{{ url('public/dist/css/adminlte.min.css') }}" rel="stylesheet">
+    <link href="{{ url('public/dist/css/custom.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
-    @vite('resources/css/app.css', 'resources/js/app.js')
+    {{-- @vite('resources/css/app.css', 'resources/js/app.js') --}}
 </head>
